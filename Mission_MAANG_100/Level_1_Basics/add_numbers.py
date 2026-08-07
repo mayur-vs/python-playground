c4643bc311ad793be9ec0a020e8304913add6ccd
Pop <pop@mail.com>
2026-08-07 11:27:39 +0530

    Level 1 Task 2 Completed

---
+++ Mission_MAANG_100/Level_1_Basics/add_numbers.py
@@ -0,0 +1,6 @@
+first_number = int(input("Enter the first number:"))
+second_number = int(input("Enter the second number:"))
+
+addition = first_number + second_number
+
+print(f"The sum of the two numbers is: {addition}")
