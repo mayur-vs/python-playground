bff3af44c496f5e932f4f6fa5ba3c30931ea9c38
Pop <pop@mail.com>
2026-08-09 17:52:19 +0530

    Task 2 Completed

---
+++ Mission_MAANG_100/Level_2_IfElse/check_even_odd.py
@@ -0,0 +1,6 @@
+user_input_number = int(input("Enter a number: "))
+
+if user_input_number % 2 == 0:
+    print(f"{user_input_number} is an even number.")
+else :
+    print(f"{user_input_number} is an odd number.")
--- Mission_MAANG_100/Level_2_IfElse/check_positive_negative.py
+++ Mission_MAANG_100/Level_2_IfElse/check_positive_negative.py
@@ -5,5 +5,7 @@ user_input = int(input("Enter a number: "))
 
 if user_input > 0 :
     print(f"{user_input} is a positive number.")
-else :
+elif user_input < 0 :
     print(f"{user_input} is a negative number.")
+else :
+    print(f"{user_input} is neither positive nor negative. It is zero.")
