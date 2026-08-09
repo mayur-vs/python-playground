c71ec631eebafdc72f9ef82d2135990927d131c9
Pop <pop@mail.com>
2026-08-09 12:12:31 +0530

    Task 6 Completed

---
+++ Mission_MAANG_100/Level_1_Basics/total_marks.py
@@ -0,0 +1,7 @@
+first_subject_marks = int(input("Enter marks for the first subject: "))
+second_subject_marks = int(input("Enter marks for the second subject: "))
+third_subject_marks = int(input("Enter marks for the third subject: "))
+
+total_marks = first_subject_marks + second_subject_marks + third_subject_marks
+
+print(f"The total marks obtained in three subjects is: {total_marks}")
