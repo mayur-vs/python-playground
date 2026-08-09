170a2ae9b7272ac673e831ac72da44a54ed54e7e
Pop <pop@mail.com>
2026-08-09 15:55:31 +0530

    Task 10 Completed

---
+++ Mission_MAANG_100/Level_1_Basics/celsius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+celsius_temperature = float(input("Enter temperature in Celsius: "))
+
+fahrenheit_temperature = (celsius_temperature * 9/5) + 32
+
+print(f"{celsius_temperature:.1f}°C is equal to {fahrenheit_temperature:.2f}°F.")
