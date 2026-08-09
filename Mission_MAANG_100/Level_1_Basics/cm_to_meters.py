1a0b516914b3424c96c00da8efe42458b0eb2409
Pop <pop@mail.com>
2026-08-09 17:37:48 +0530

    Task 24 and Task 25 Completed

---
+++ Mission_MAANG_100/Level_1_Basics/cm_to_meters.py
@@ -0,0 +1,5 @@
+users_height_in_cm = float(input("Enter your height in centimeters: "))
+
+users_height_in_m = users_height_in_cm / 100
+
+print(f"Your height in meters is: {users_height_in_m:.2f}")
---
+++ Mission_MAANG_100/Level_1_Basics/fav_actor.py
@@ -0,0 +1,3 @@
+actor_name = input("Enter your favorite actor's name: ")
+
+print(f"Your favorite actor is: {actor_name}")
