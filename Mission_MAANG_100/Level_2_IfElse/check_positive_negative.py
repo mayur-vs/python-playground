c9cc3b8cbc489df1582638b34ba3e76d6a1773cf
Pop <pop@mail.com>
2026-08-09 17:47:40 +0530

    Level 2 Task 1 Completed

---
+++ Mission_MAANG_100/Level_2_IfElse/check_positive_negative.py
@@ -0,0 +1,9 @@
+# Yahan se computer aapse sirf math nahi karega,
+# balki sochna shuru karega aur conditions ke hisaab se faisle lega!
+
+user_input = int(input("Enter a number: "))
+
+if user_input > 0 :
+    print(f"{user_input} is a positive number.")
+else :
+    print(f"{user_input} is a negative number.")
