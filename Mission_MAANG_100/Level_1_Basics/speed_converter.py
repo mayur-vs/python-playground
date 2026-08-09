f080c88aaa67fa8a7b40d30d2665a780eefb4eb3
Pop <pop@mail.com>
2026-08-09 16:43:15 +0530

    Task 18 Completed

--- Mission_MAANG_100/Level_1_Basics/speed_converter.py
+++ Mission_MAANG_100/Level_1_Basics/speed_converter.py
@@ -2,4 +2,4 @@ car_speed_kmh = float(input("Enter the speed of the card in km/h: "))
 
 car_speed_mps = car_speed_kmh * 1000 / 3600
 
-print(f"The speed of the car in m/s is: {car_speed_mps}.")
+print(f"The speed of the car in m/s is: {car_speed_mps:.2f}")
