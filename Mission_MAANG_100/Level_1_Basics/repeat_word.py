02a9a5c699e3799b3a905b7222f9feac620a6c5e
Pop <pop@mail.com>
2026-08-09 16:32:55 +0530

    Task 15 & Task 16 Completed

---
+++ Mission_MAANG_100/Level_1_Basics/repeat_word.py
@@ -0,0 +1,5 @@
+user_word = input("Enter a word: ")
+
+result = user_word * 3
+
+print(f"The word '{user_word}' repeated 3 times is: {result}.")
---
+++ Mission_MAANG_100/Level_1_Basics/swap_variables.py
@@ -0,0 +1,6 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+a, b = b, a
+
+print(f"After swapping, the first number is {a} and the second number is {b}.")
