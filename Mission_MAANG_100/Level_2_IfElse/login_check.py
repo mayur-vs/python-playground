d8d34b6282d31fe1943f0d4bb73ceaa879a5de7b
Pop <pop@mail.com>
2026-08-10 11:37:32 +0530

    Task 8 Completed

---
+++ Mission_MAANG_100/Level_2_IfElse/login_check.py
@@ -0,0 +1,6 @@
+user_input_password = input("Enter your password: ")
+
+if user_input_password == "admin123" :
+    print("Login Successful!")
+else :
+    print("Login Failed! Please check your password and try again.")
