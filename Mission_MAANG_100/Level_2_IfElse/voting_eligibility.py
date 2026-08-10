4945f9c4a9d73209336f9c97de8f84a22df8d414
Pop <pop@mail.com>
2026-08-10 11:00:03 +0530

    Level 2 Task 2 Completed

---
+++ Mission_MAANG_100/Level_2_IfElse/voting_eligibility.py
@@ -0,0 +1,6 @@
+user_age_input = int(input("Enter your age: "))
+
+if user_age_input >= 18:
+    print(f"You are eligible to vote.")
+else :
+    print(f"You are not eligible to vote. You need to be at least 18 years old.")
