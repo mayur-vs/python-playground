8475c422e6fce332970ba2629a2ea7c754e4a6fc
Pop <pop@mail.com>
2026-08-11 11:07:42 +0530

    Task 15 Completed

---
+++ Mission_MAANG_100/Level_2_IfElse/check_empty_string.py
@@ -0,0 +1,6 @@
+user_input_string = input("Enter the Text: ")
+
+if user_input_string == "" :
+    print(f"You entered an empty text")
+else :
+    print(f"You entered a {user_input_string} text")
--- Mission_MAANG_100/Level_2_IfElse/divisible_by_2_and_3.py
+++ Mission_MAANG_100/Level_2_IfElse/divisible_by_2_and_3.py
@@ -1,6 +1,8 @@
 user_input_number = int(input('Enter the number: '))
 
-if (user_input_number % 2 == 0) and (user_input_number % 3 == 0) :
+# Least Common Multiple of 2 and 3 is 6, so we can check directly with 6
+# if number is dividing 6 then it is obviously go to divide 2 and 3
+if user_input_number % 6 == 0 :
     print(f"{user_input_number} is divisible by 2 and 3")
 else :
     print(f"{user_input_number} is not divisible by 2 and 3")
