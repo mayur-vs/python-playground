de3fa3437e149edeed39c3f354af89581448fa16
Pop <pop@mail.com>
2026-08-17 18:49:49 +0530

    Adding files

---
+++ Mission_MAANG_100/Level_1_Basics/take_input_from_user.py
@@ -0,0 +1,9 @@
+# Step 1: Take input from the user
+user_first_number = int(input("Enter the first number: "))
+user_second_number = int(input("Enter the second number: "))
+
+# Step 2: Perform addition
+result = user_first_number + user_second_number
+
+# Step 3: Display the result
+print("The sum of the two numbers is:", result)
